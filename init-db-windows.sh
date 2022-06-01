3d6479d1db20333a27ee1dc7b6a01997fabcf198
Gil <joaogilfernandes@ua.pt>
2022-06-01 23:28:18 +0100

    init-db-windows

---
+++ init-db-windows.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $# != 4 ]] ; then
+	echo "Usage: $0 db_host db_username db_password db_name"
+    exit
+fi
+
+echo "Creating secret.php file..."
+
+SECRET_FILE=php/secret.php
+touch ${SECRET_FILE}
+
+echo "<?php" > ${SECRET_FILE}
+echo "\$db_host='$1';" >> ${SECRET_FILE}
+echo "\$db_username='$2';" >> ${SECRET_FILE}
+echo "\$db_password='$3';" >> ${SECRET_FILE}
+echo "\$db_name='$4';" >> ${SECRET_FILE}
+
+echo "Adding database '$4'..."
+
+sudo mkdir /mnt/c/xampp/mysql/data/$4
+sudo chmod -R a+rwx /mnt/c/xampp/mysql/data/$4
+
+echo "Adding tables:"
+
+response=$(curl -sX GET localhost:80/php/setup-db.php)
+eol=$'\n'
+response="${response//<br>/$eol}"
+echo "$response"
