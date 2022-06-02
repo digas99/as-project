e222af813b8594d29d0be5eeaf24ed5cd86aceeb
Goncalo <goncalorodrigues@ua.pt>
2022-06-02 19:20:51 +0100

    inital commit front-end

--- index.php
+++ index.php
@@ -5,10 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Gamebot</title>
+    <title>GameBet</title>
 </head>
 
 <body>
+    <nav class="nav">
+        <!--Individual classes on the links-->
+        <a href="/pages/home.html" class="nav__link" link>Home</a>
+        <a href="/pages/search.html" class="nav__link" link>Search</a>
+
+    </nav>
+
+
+    <!--script type="module" src="/js/index.js"></script-->
+    <!-- <link rel="shortcut icon" href="#"> -> favicon.cio-->
+    
   
 </body>
 </html>
