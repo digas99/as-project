63a0dd54e4a1ae7661ba420e98ebb97053032abe
Goncalo <goncalo.rodrigues@ua.pt>
2022-06-03 17:07:41 +0100

    more home stuff

---
+++ wallet.php
@@ -0,0 +1,19 @@
+<head>
+  <link rel="stylesheet" href="css/wallet.css"/>
+  <link rel="stylesheet" href="css/navbar.css"/> 
+</head>
+
+<body>
+    <?php include "templates/navbar.php"?>
+
+    <h1 id="walletTitle">Wallet</h1>
+
+
+
+
+
+
+
+    <script type="text/javascript" src="js/navbar.js"></script>
+    <script type="text/javascript" src="js/wallet.js"></script>
+</body>
